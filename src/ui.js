80e14e702a15e6718f83298df3074263d152a678
Ryan Blue <ryanzblue@gmail.com>
2019-01-30 21:43:36 -0500

    Update camera stream view when NT updates list Reformat code

--- src/ui.js
+++ src/ui.js
@@ -25,18 +25,17 @@ let ui = {
 ui.camera.setStreamURL = (url) => {
     ui.camera.style.backgroundImage = `url(${url}), url('../images/stream_not_found.png')`
 };
+let streamList = new Map();
+ui.cameraSelector.getSelectedStream = () => {
+    return streamList.get(ui.cameraSelector.value);
+};
+
 //Timer
 NetworkTables.addKeyListener('/Robot/Time', (key, value) => {
     // This is an example of how a dashboard could display the remaining time in a match.
     // We assume here that value is an integer representing the number of seconds left.
     ui.timer.innerHTML = value < 0 ? '0:00' : Math.floor(value / 60) + ':' + (value % 60 < 10 ? '0' : '') + value % 60;
 });
-
-//Camera stream updates
-NetworkTables.addKeyListener('/SmartDashboard/StreamURL', (key, value) => {
-    ui.camera.setStreamURL(value);
-});
-
 
 // Key Listeners
 // Load list of prewritten autonomous modes
@@ -47,7 +46,7 @@ NetworkTables.addKeyListener('/SmartDashboard/Auto List', (key, value) => {
     }
     console.log(value.length);
     // Make an option for each autonomous mode and put it in the selector
-    if(value.length < 1) value[0] = "No auto modes found";
+    if (value.length < 1) value[0] = "No auto modes found";
     value.forEach((mode) => {
         let option = document.createElement('option');
         option.appendChild(document.createTextNode(mode));
@@ -61,7 +60,7 @@ NetworkTables.addKeyListener('/SmartDashboard/Auto List', (key, value) => {
 NetworkTables.addKeyListener('/SmartDashboard/Selected Auto', (key, value) => {
     ui.autoConfirm.value = value;
     ui.autoSelect.value = value;
-    if(ui.autoSelect.value == value) {
+    if (ui.autoSelect.value == value) {
         ui.autoConfirm.style.backgroundColor = 'green';
     } else {
         ui.autoConfirm.style.backgroundColor = 'red';
@@ -71,20 +70,28 @@ NetworkTables.addKeyListener('/SmartDashboard/Selected Auto', (key, value) => {
 ui.autoSelect.addEventListener('change', (event) => {
     NetworkTables.putValue('/SmartDashboard/Selected Auto', event.target.value);
 });
-
 NetworkTables.addKeyListener('/FMSInfo/EventName', (key, value) => {
     ui.matchInfo.event.innerHTML = value;
 });
 NetworkTables.addKeyListener('/FMSInfo/MatchNumber', (key, value) => {
     ui.matchInfo.match.number.innerHTML = value;
 });
+
 NetworkTables.addKeyListener('/FMSInfo/MatchType', (key, value) => {
     let matchTypeString;
-    switch(value) {
-        case 1: matchTypeString = 'Practice'; break;
-        case 2: matchTypeString = 'Qualification'; break;
-        case 3: matchTypeString = 'Elimination'; break;
-        default: matchTypeString = 'Unknown'; break;
+    switch (value) {
+        case 1:
+            matchTypeString = 'Practice';
+            break;
+        case 2:
+            matchTypeString = 'Qualification';
+            break;
+        case 3:
+            matchTypeString = 'Elimination';
+            break;
+        default:
+            matchTypeString = 'Unknown';
+            break;
     }
     ui.matchInfo.match.type.innerHTML = matchTypeString + " ";
 });
@@ -93,8 +100,8 @@ NetworkTables.addKeyListener('/Robot/BatteryVoltage', (key, value) => {
     value = Number.parseFloat(value).toFixed(2);
     ui.batteryPanel.readout.innerHTML = value;
     let color;
-    if(value < 7) color = 'red';
-    else if(value < 10) color = 'yellow';
+    if (value < 7) color = 'red';
+    else if (value < 10) color = 'yellow';
     else color = 'green';
     ui.batteryPanel.panel.style.color = color === 'yellow' ? 'black' : 'white';
     ui.batteryPanel.panel.style.backgroundColor = color;
@@ -112,41 +119,48 @@ NetworkTables.addKeyListener('/Robot/BatteryVoltage', (key, value) => {
         ui.robotState.modeIndicator.innerHTML = modeString;
     }
 });*/
-addEventListener('error',(ev)=>{
-    ipc.send('windowError',{mesg:ev.message,file:ev.filename,lineNumber:ev.lineno})
-});
-
-addEventListener('keydown', evt => {
-    if(evt.key === 'w' && evt.ctrlKey) electron.getCurrentWindow().close();
-});
 
 NetworkTables.addGlobalListener((key, value, isNew) => {
-    if (!key.startsWith('/CameraPublisher') || !isNew) return;
+    console.log(key);
+    if (!key.startsWith('/CameraPublisher')) return;
     let stream = key.split('/')[2];
     updateStream(getStream(stream));
     updateStreamList();
 });
-let streamList = new Map();
-ui.cameraSelector.getSelectedStream = () => {
-    return streamList.get(ui.cameraSelector.value);
-};
 
-ui.cameraSelector.addEventListener('change', () => {
+//Camera stream updates
+NetworkTables.addKeyListener('/SmartDashboard/StreamURL', (key, value) => {
+    ui.camera.setStreamURL(value);
+});
+
+let updateCameraStreamView = () => {
     let streamUrl = ui.cameraSelector.getSelectedStream().streams[0];
-    if(typeof streamUrl != 'undefined') {
+    if (typeof streamUrl != 'undefined') {
         ui.camera.setStreamURL(streamUrl);
     }
-});
+};
+
+ui.cameraSelector.addEventListener('change', updateCameraStreamView);
+
 let updateStream = (streamToUpdate) => {
     streamList.set(streamToUpdate.id, streamToUpdate);
 };
+
 let updateStreamList = () => {
-    while(ui.cameraSelector.firstChild) ui.cameraSelector.removeChild(ui.cameraSelector.firstChild);
+    //Clear the selector
+    while (ui.cameraSelector.firstChild) ui.cameraSelector.removeChild(ui.cameraSelector.firstChild);
+    //If empty put a message
+    if (streamList.size === 0) {
+        let option = document.createElement('option');
+        option.text = 'No streams available';
+        ui.cameraSelector.add(option);
+    }
     streamList.forEach((stream, id) => {
         let option = document.createElement('option');
         option.text = id;
         ui.cameraSelector.add(option);
-    })
+    });
+    updateCameraStreamView();
 };
 
 let getStream = (id) => {
@@ -161,6 +175,15 @@ let getStream = (id) => {
         availableModes: getStreamSubKey(id, 'modes', [])
     };
 };
+
 let getStreamSubKey = (id, key, defaultVal) => {
     return NetworkTables.getValue(`/CameraPublisher/${id}/${key}`, defaultVal);
 };
+
+addEventListener('error', (ev) => {
+    ipc.send('windowError', {mesg: ev.message, file: ev.filename, lineNumber: ev.lineno})
+});
+
+addEventListener('keydown', evt => {
+    if (evt.key === 'w' && evt.ctrlKey) electron.getCurrentWindow().close();
+});
